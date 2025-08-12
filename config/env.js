2778cf9cf2158762af6099623471b29dcb8fffa4
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-08-12 21:52:28 +0545

    some necessary comments

--- config/env.js
+++ config/env.js
@@ -3,4 +3,5 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
+//destructuring process.env to get the variables
 export const { PORT, NODE_ENV, DB_URL } = process.env;
