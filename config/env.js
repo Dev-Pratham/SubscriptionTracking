948838c067481931ebb7849fad24f436077bec9b
Dev-Pratham <pratham.chaurasia31@gmail.com>
2026-01-18 00:48:07 +0530

    fixed .env

--- config/env.js
+++ config/env.js
@@ -3,5 +3,12 @@ import { config } from "dotenv";
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
 //destructuring process.env to get the variables
-export const { PORT, NODE_ENV, DB_URL, JWT_SECRET, JWT_EXPIRES_IN, token, id } =
-  process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URL,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_ENV,
+  ARCJET_KEY,
+} = process.env;
